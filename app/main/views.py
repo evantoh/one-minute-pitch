67e3687d2bf8ed9c87f03fa754f76b79478b570c
evantoh <evanmwenda@gmail.com>
2018-02-05 17:46:49 +0300

    add update.html file

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,7 @@ from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..models import User
 from ..import db
+from  .forms import UpdateProfile
 from flask_login import login_required  
 
 
@@ -13,10 +14,29 @@ def index():
     return render_template('index.html',title = title)
 
 @main.route('/user/<uname>')
-def profile(uname)
-    user = User,query.filter_by(username = uname).first()
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
 
     if user is None:
         abort(404)
 
     return render_template("profile/profile.html",user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
