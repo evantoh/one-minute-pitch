7bd742904766d743a837f3a8c2f71f9bf0caf846
evantoh <evanmwenda@gmail.com>
2018-02-05 09:50:23 +0300

    add title of the application

--- app/views.py
+++ app/views.py
@@ -4,4 +4,5 @@ from app import app
 #views
 @app.route('/')
 def index():
-    return render_template('index.html')
+    title = 'one minute pitch'
+    return render_template('index.html',title = title)
