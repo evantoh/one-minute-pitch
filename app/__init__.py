854c57658e845fe973f245520568d856350dc7d9
evantoh <evanmwenda@gmail.com>
2018-02-05 16:31:11 +0300

    configuring the models

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_login import LoginManager
 
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
@@ -26,6 +30,9 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
 
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
     #Registering the blueprint
     from .main import main as main_blueprint
--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,11 @@
 from flask import render_template,request,redirect,url_for
 from . import main
+from flask_login import login_required  
 
 
 #views
-@main.route('/')
+@main.route('/',methods = ['GET','POST'])
+@login_required
 def index():
     title = 'one minute pitch'
     return render_template('index.html',title = title)
