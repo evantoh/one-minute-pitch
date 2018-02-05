5df3126993991e2ae6b008dcd1b22267cfec791d
evantoh <evanmwenda@gmail.com>
2018-02-05 13:05:56 +0300

    add class users

--- config.py
+++ config.py
@@ -2,30 +2,19 @@ import os
 
 class Config:
 
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://evans:evans123@localhost/watchlist'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
-    #email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    UPLOADED_PHOTOS_DEST='app/static/photos'
+    
 class ProdConfig(Config):
-    pass
 
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://evans:evans123@localhost/watchlist'
-    DEBUG = True
 
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://evans:evans123@localhost/watchlist_test'
-
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://evans:evans123@localhost/one_minute_pitch'
+    DEBUG = True    
 
 config_options = {
 'development':DevConfig,
